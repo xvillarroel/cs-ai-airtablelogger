3d373eaab46ea6f1396eddea084505a109aeb78a
xvillarroel <xaviervillarroelparra@gmail.com>
2024-02-26 16:46:25 -0300

    added dynamo_table_name

--- index.mjs
+++ index.mjs
@@ -6,6 +6,7 @@ const ddbClient = new DynamoDBClient({ region: "us-east-1" }); // Asegúrate de 
 const globals = {
     DEFAULT_BASE_ID: "apppHKPOOlTMWd8C4",
     DEFAULT_TABLE_ID: "tblSMmFtgOdQD5W9w",
+    DEFAULT_DYNAMO_TABLE_NAME: "voiceflow-twilio-collections-logs",
     AIRTABLE_TOKEN: "pat562IRmmXUsUbdj.a2ad65922100ce3d92b9cb1141d1bff4b9b53b8ba51378eb5950397ea9662726",
     ROWOFFSET: 1,
 };
@@ -56,10 +57,10 @@ const writeToAirtable = async (token, baseID, tableID, dataToWrite) => {
 
 }
 
-const writeToDynamoDB = async (dataToWrite) => {
+const writeToDynamoDB = async (dataToWrite, tableName) => {
     console.log(`A. writeToDynamoDB first line`)
     const params = {
-        TableName: "voiceflow-twilio-collections-logs",
+        TableName: tableName,
         Item: {
             //"PrimaryKey": { S: "Timeframe" }, // Cambia "PrimaryKey" y su valor según tu diseño de tabla
             "Timeframe": { S: dataToWrite.Timeframe },
@@ -84,43 +85,28 @@ export const handler = async (event, context) => {
     console.log(JSON.stringify(event,null,2));
     console.log('*************************************')
 
-    let eventBody;
-    if (event.body) {
-        eventBody = JSON.parse(event.body);
-    } else { 
-        return await assembleResponse(400,'This reqeust has no payload.'); 
-    }
-
-    // UNIVERSAL VALIDATOR OF METHOD
     if (event.requestContext.http.method !== 'POST') {
-        let res = {
-            statusCode: 400,
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8'
-            },
-            body: "Method is not allowed."
-        }
-        console.log(JSON.stringify(res, null, 2))
-        return res;
+        return await assembleResponse(400,'Method is not allowed'); 
     }
-    // UNIVERSAL VALIDATOR OF METHOD
-    console.log('1. Universal validator of method executed.')
 
-    let message;
-    if (!eventBody.Message) { 
+    let eventBody;
+        if (event.body) {
+            eventBody = JSON.parse(event.body);
+        } else { 
+            return await assembleResponse(400,'This reqeust has no payload.'); 
+        }
+
+    let message; 
         if (!eventBody.message) {
-            return await assembleResponse(400,'Message is missing.');
+            return await assembleResponse(400, 'Message is missing.');
         } else {
             message = eventBody.message;
         }
-    } else {
-        message = eventBody.Message;
-    }
 
-    let baseID = (!eventBody.base_id) ? globals.DEFAULT_BASE_ID : eventBody.base_id;
-    let tableID = (!eventBody.table_id) ? globals.DEFAULT_TABLE_ID : eventBody.table_id; 
+    let baseID = (!eventBody.airtable_base_id) ? globals.DEFAULT_BASE_ID : eventBody.base_id; //Only for AIRTABLE logging.
+    let tableID = (!eventBody.airtable_table_id) ? globals.DEFAULT_TABLE_ID : eventBody.table_id; //Only for AIRTABLE logging.
+    let tableName = (!eventBody.dynamo_table_name) ? globals.DEFAULT_DYNAMO_TABLE_NAME : eventBody.dynamo_table_name; //Only for DYNAMO logging.;
     let sessionID = (!eventBody.session) ? "No Session Specified" : eventBody.session; 
-    
     let timeframe = getCurrentDate(false) // get the full timeframe, not only the DD/MM/YYYY
 
     console.log('2. Variables assigned.')
@@ -129,10 +115,11 @@ export const handler = async (event, context) => {
     {
         "Timeframe": timeframe,
         "Message": message,
-        "Session": sessionID
+        "Session": sessionID,
+        "DynamoID": 
     };
 
-    await writeToDynamoDB (information);
+    await writeToDynamoDB (information, tableName);
     await writeToAirtable (globals.AIRTABLE_TOKEN, baseID, tableID, information);
 
     console.log('3. Dynamo functino called.')
